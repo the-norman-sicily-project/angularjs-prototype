3baefd19a700afb30049d845f73863b2e773c1dd
Joe Hayes <joephayes@gmail.com>
2015-09-22 00:06:52 -0400

    new schema

--- client/app/sites/sites.controller.js
+++ client/app/sites/sites.controller.js
@@ -3,7 +3,7 @@
 angular.module('siciliaNormannaApp')
   .controller('SitesController', function ($scope, sitesData) {
     $scope.sites= sitesData;
-    $scope.siteTypes = _.groupBy($scope.sites, 'siteType');
+    $scope.siteTypes = _.groupBy($scope.sites, 'type');
   })
   .controller('SiteController', function ($scope, siteData) {
     $scope.site = siteData;
--- server/api/site/site.model.js
+++ server/api/site/site.model.js
@@ -4,9 +4,19 @@ var mongoose = require('bluebird').promisifyAll(require('mongoose'));
 var Schema = mongoose.Schema;
 
 var SiteSchema = new Schema({
-  siteName: String,
-  modernProvince: String,
-  siteType: String
+  name: {
+    en: String,
+    it: String
+  },
+  location: {
+      longitude: Number,
+      latitude: Number,
+      isExact: Boolean,
+      modernProvince: String,
+      approximate:String,
+      elevation:Number
+  },
+  type: String
 });
 
 module.exports = mongoose.model('Site', SiteSchema);
