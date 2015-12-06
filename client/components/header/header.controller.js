0808622de0c72dde044bda4f55c77ffdd3f888d8
Joe Hayes <joephayes@gmail.com>
2015-12-06 00:13:21 -0500

    wip - header and footer rework

---
+++ client/components/header/header.controller.js
@@ -0,0 +1,7 @@
+'use strict';
+
+angular.module('siciliaNormannaApp')
+  .controller('HeaderController', function ($scope) {
+      $scope.title = 'The Norman Sicily Project';
+      $scope.subtitle = 'Envisioning the Norman State in Southern Italy, c. 1061 - 1194';
+  });
---
+++ client/components/header/header.directive.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('siciliaNormannaApp')
+  .directive('header', function () {
+    return {
+      templateUrl: 'components/header/header.html',
+      restrict: 'E',
+      controller: 'HeaderController'
+    };
+  });
