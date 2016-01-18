86ad1e13abc0d9d722282eb007e180600946f364
Joe Hayes <joephayes@gmail.com>
2016-01-17 21:17:31 -0500

    remove 'thing' from server side API

--- server/routes.js
+++ server/routes.js
@@ -10,7 +10,6 @@ import path from 'path';
 export default function(app){
   // Insert routes below
   app.use('/api/sites', require('./api/site'));
-  app.use('/api/things', require('./api/thing'));
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
    .get(errors[404]);
