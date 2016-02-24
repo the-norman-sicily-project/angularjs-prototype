4e10a3f528d08ca5097fc7bd41d5eb71491aa16b
Joe Hayes <joephayes@gmail.com>
2016-02-23 22:55:54 -0500

    Add pluralize tests and tweaks to pluralize

--- client/components/pluralize/pluralize.filter.js
+++ client/components/pluralize/pluralize.filter.js
@@ -5,22 +5,22 @@
 angular.module('siciliaNormannaApp')
   .filter('pluralize', function () {
     return function (ordinal, noun) {
-      if (ordinal == 1) {
-        return noun;
-      } else {
+      if (ordinal > 1 && noun.length > 0) {
         var plural = noun;
-        if (noun.substr(noun.length - 2) == 'us') {
+        if (noun.substr(noun.length - 2) === 'us') {
           plural = plural.substr(0, plural.length - 2) + 'i';
-        } else if (noun.substr(noun.length - 2) == 'ch' || noun.charAt(noun.length - 1) == 'x' || noun.charAt(noun.length - 1) == 's') {
-          plural += 'es';
-        } else if (noun.charAt(noun.length - 1) == 'y' && ['a','e','i','o','u'].indexOf(noun.charAt(noun.length - 2)) == -1) {
-          plural = plural.substr(0, plural.length - 1) + 'ies';
-        } else if (noun.substr(noun.length - 2) == 'is') {
+        } else if (noun.substr(noun.length - 2) === 'is') {
           plural = plural.substr(0, plural.length - 2) + 'es';
+        } else if (noun.substr(noun.length - 2) === 'ch' || noun.charAt(noun.length - 1) === 'x' || noun.charAt(noun.length - 1) === 's') {
+          plural += 'es';
+        } else if (noun.charAt(noun.length - 1) === 'y' && ['a','e','i','o','u'].indexOf(noun.charAt(noun.length - 2)) === -1) {
+          plural = plural.substr(0, plural.length - 1) + 'ies';
         } else {
           plural += 's';
         }
         return plural;
+      } else {
+        return noun;
       }
     };
   });
---
+++ client/components/pluralize/pluralize.filter.spec.js
@@ -0,0 +1,69 @@
+'use strict';
+
+describe('Filter: pluralize', function () {
+
+  // load the filter's module
+  beforeEach(module('siciliaNormannaApp'));
+
+  // initialize a new instance of the filter before each test
+  var pluralize;
+  beforeEach(inject(function ($filter) {
+    pluralize = $filter('pluralize');
+  }));
+
+  it('should correctly pluralize word ending in -ch"', function () {
+    var text = 'church';
+    expect(pluralize(2, text)).to.equal(text + 'es');
+  });
+
+  it('should correctly pluralize word ending in -x"', function () {
+    var text = 'tax';
+    expect(pluralize(2, text)).to.equal(text + 'es');
+  });
+
+  it('should correctly pluralize word ending in -s"', function () {
+    var text = 'boss';
+    expect(pluralize(2, text)).to.equal(text + 'es');
+  });
+
+  it('should correctly pluralize word ending in -us"', function () {
+    var text = 'virus';
+    expect(pluralize(2, text)).to.equal(text.substr(0, text.length -2) + 'i');
+  });
+
+  it('should correctly pluralize word ending in -is"', function () {
+    var text = 'basis';
+    expect(pluralize(2, text)).to.equal(text.substr(0, text.length - 2) + 'es');
+  });
+
+  it('should correctly pluralize word ending in -y preceded by a consonant', function () {
+    var text = 'lady';
+    expect(pluralize(2, text)).to.equal(text.substr(0, text.length - 1) + 'ies');
+  });
+
+  it('should correctly pluralize word ending in -y preceded by a vowel', function () {
+    var text = 'trolley';
+    expect(pluralize(2, text)).to.equal(text + 's');
+  });
+
+  it('should correctly pluralize regular word', function () {
+    var text = 'hedge';
+    expect(pluralize(2, text)).to.equal(text + 's');
+  });
+
+  it('should return unchanged word if ordinal is 1', function() {
+    var text = 'car';
+    expect(pluralize(1, text)).to.equal(text);
+  });
+
+  it('should return unchanged word if ordinal is 0', function(){
+    var text = 'car';
+    expect(pluralize(0, text)).to.equal(text);
+  });
+
+  it('should return empty string if given one, regardless of ordinal', function(){
+    var text = '';
+    expect(pluralize(1, text)).to.equal(text);
+    expect(pluralize(1, text)).to.equal(text);
+  });
+});
