8ef4729b07a6f8d15465701b6223d792502449c6
Joe Hayes <joephayes@gmail.com>
2016-03-05 21:02:31 -0500

    Add cloudinary npm package Make site virtual properties getSlides, getVideos and getDocuments instance methods

--- server/api/site/site.model.js
+++ server/api/site/site.model.js
@@ -2,9 +2,7 @@
 
 var mongoose = require('bluebird').promisifyAll(require('mongoose'));
 var Schema = mongoose.Schema;
-var fs = require('fs');
-var _ = require('lodash');
-var path = require('path');
+var cloudinary = require('cloudinary');
 
 var SiteSchema = new Schema({
   name: {
@@ -32,78 +30,21 @@ var SiteSchema = new Schema({
   toJSON: { virtuals: true }
 });
 
-SiteSchema
-  .virtual('slides')
-  .get(function() {
-    var slides = [];
-    // images are assumed to be kept in a single directory per site with no
-    // subdirectories. Only image files will be kept in these directories
-    // (no listing files, etc.)
-    var siteImagesPath = path.join(__dirname, `/../../media/images/sites/${this._id.toString()}`);
-    try {
-      fs.statSync(siteImagesPath);
-      var files = fs.readdirSync(siteImagesPath);
-      slides = files.map(function(file) {
-        if (file !== '.DS_Store') {
-          return {filename: file};
-        }
-        return null;
-      });
-    } catch(err) {
-      //console.log('Directory ' + siteImagesPath + ' does not exist!');
-    }
-    return slides;
-  });
+SiteSchema.methods.getSlides = function getSlides(cb) {
+  cloudinary.api.resources({type: 'upload',
+    prefix: `${this.media_id.toString()}/`},
+    function(error, result) {
+      cb(error, result);
+    });
+};
 
-SiteSchema
-  .virtual('videos')
-  .get(function() {
-    var videos = [];
-    // videos are assumed to be kept in a single directory per site with no
-    // subdirectories. Only video files will be kept in these directories
-    // (no listing files, etc.)
-    var siteVideosPath = path.join(__dirname, `/../../media/videos/sites/${this._id.toString()}`);
-    var siteVideoTitle = this.name.en;
-    try {
-      fs.statSync(siteVideosPath);
-      var files = fs.readdirSync(siteVideosPath);
-      videos = _.compact(files.map(function(file) {
-        if (file !== '.DS_Store') {
-          return {filename: file, title: siteVideoTitle};
-        }
-        return null;
-      }));
-    } catch(err) {
-      //console.log('Directory ' + siteVideosPath + ' does not exist!');
-    }
+SiteSchema.methods.getVideos = function getVideos(cb) {
+  cb(null, null);
+};
 
-    return videos;
-  });
-
-SiteSchema
-  .virtual('documents')
-  .get(function() {
-    var documents = [];
-    // documents are assumed to be kept in a single directory per site with no
-    // subdirectories. Only video files will be kept in these directories
-    // (no listing files, etc.)
-    var siteDocumentsPath = path.join(__dirname, `/../../media/documents/sites/${this._id.toString()}`);
-    var siteDocumentTitle = this.name.en;
-    try {
-      fs.statSync(siteDocumentsPath);
-      var files = fs.readdirSync(siteDocumentsPath);
-      documents = _.compact(files.map(function(file) {
-        if (file !== '.DS_Store') {
-          return {filename: file, title: siteDocumentTitle};
-        }
-        return null;
-      }));
-    } catch(err) {
-      //console.log('Directory ' + siteDocumentsPath + ' does not exist!');
-    }
-
-    return documents;
-  });
+SiteSchema.methods.getDocuments = function getDocuments(cb) {
+  cb(null, null);
+};
 
 SiteSchema
   .virtual('tooltipText')
