075eff28fdfcc5bd4305b6194b3d2d086521c21c
Joe Hayes <joephayes@gmail.com>
2016-10-06 00:44:50 -0400

    fix youtube directive spec

--- client/components/youtube/youtube.directive.spec.js
+++ client/components/youtube/youtube.directive.spec.js
@@ -11,9 +11,11 @@ describe('Directive: youtube', function() {
     scope = $rootScope.$new();
   }));
 
-  it('should make hidden element visible', inject(function($compile) {
-    element = angular.element('<youtube></youtube>');
+  it('should use scope code variable to construct youtube URL', inject(function($compile) {
+    element = angular.element('<youtube code="code"></youtube>');
+    scope.code = 'AAA111BBB';
     element = $compile(element)(scope);
-    expect(element.text()).to.equal('this is the youtube directive');
+    scope.$digest();
+    expect(element.find('iframe').attr('src')).to.contain(scope.code);
   }));
 });
