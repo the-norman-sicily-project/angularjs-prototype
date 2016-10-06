609e1bc739d3b42cb01fa28128ac22055c162e40
Joe Hayes <joephayes@gmail.com>
2016-10-06 07:37:50 -0400

    Use random string generator for testing youtube directive

--- client/components/youtube/youtube.directive.spec.js
+++ client/components/youtube/youtube.directive.spec.js
@@ -5,15 +5,26 @@ describe('Directive: youtube', function() {
   beforeEach(module('siciliaNormannaApp'));
 
   var element,
-    scope;
+    scope,
+    randomString;
 
   beforeEach(inject(function($rootScope) {
     scope = $rootScope.$new();
+    randomString = (length, chars) => {
+      var mask = '';
+      if (chars.indexOf('a') > -1) mask += 'abcdefghijklmnopqrstuvwxyz';
+      if (chars.indexOf('A') > -1) mask += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+      if (chars.indexOf('#') > -1) mask += '0123456789';
+      if (chars.indexOf('!') > -1) mask += '~`!@#$%^&*()_+-={}[]:";\'<>?,./|\\';
+      var result = '';
+      for (var i = length; i > 0; --i) result += mask[Math.floor(Math.random() * mask.length)];
+      return result;
+    };
   }));
 
   it('should use scope code variable to construct youtube URL', inject(function($compile) {
     element = angular.element('<youtube code="code"></youtube>');
-    scope.code = 'AAA111BBB';
+    scope.code = randomString(10, 'aA#!');
     element = $compile(element)(scope);
     scope.$digest();
     expect(element.find('iframe').attr('src')).to.contain(scope.code);
