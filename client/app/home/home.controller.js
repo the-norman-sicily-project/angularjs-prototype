08013a072aed4a646a56553d380f74b44e49983e
Joe Hayes <joephayes@gmail.com>
2016-10-05 23:29:33 -0400

    Update to correct youtube video id code add back popover for idrisi map

--- client/app/home/home.controller.js
+++ client/app/home/home.controller.js
@@ -3,7 +3,7 @@
 class HomeController {
   constructor($scope, $sce) {
     this.idrisiMapPopoverContent = $sce.trustAsHtml('Detail from Muhammad al-Idrīsī\'s world map contained in the twelfth-century <em>Kitāb nuzhat al-mushtāq fī ikhtirāq al-āfāq</em> (<em>The Pleasure Excursion of One Who Is Eager to Traverse the Regions of the World</em>), whose title Idrīsī later changed to <em>Kitab Rujar</em> (<em>Book of Roger</em>), which contained what was perhaps the most accurate map of the medieval world. The selection is an illustration of Sicily and Southern Italy. The orientation appears to be upside down because south is located at the top of the map in accordance with the cartographic conventions of the <em>Balkhī</em> school of geography.');
-    this.code = "IogaeqeNkW8";
+    this.code = "CZYBeWEAeC4";
   }
 }
 
--- client/components/youtube/youtube.directive.js
+++ client/components/youtube/youtube.directive.js
@@ -7,7 +7,8 @@ class YouTubeDirective {
     this.replace = true;
     this.template = '<div class="center-block" style="width:711px;height:400px;"><iframe style="overflow:hidden;height:100%;width:100%" width="100%" height="100%" src="{{url}}" frameborder="0" allowfullscreen></iframe></div>';
     this.link = scope => {
-      scope.url = $sce.trustAsResourceUrl(`http://www.youtube.com/embed/${scope.$parent.home.code}?rel=0&modestbranding=1&showinfo=0`);
+      scope.url =
+        $sce.trustAsResourceUrl(`http://www.youtube.com/embed/${scope.$parent.home.code}?rel=0&modestbranding=1&showinfo=0`);
     };
   };
 }
