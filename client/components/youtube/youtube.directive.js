8d509a24fccf0181d89f7abe2c0808d2051cf068
Joe Hayes <joephayes@gmail.com>
2016-10-05 23:51:23 -0400

    stop using reference to $parent in directive

--- client/components/youtube/youtube.directive.js
+++ client/components/youtube/youtube.directive.js
@@ -3,12 +3,15 @@
 class YouTubeDirective {
   constructor($sce) {
     this.restrict = 'EA';
-    this.scope = { code: '='};
+    this.scope = { code: '=code'};
     this.replace = true;
     this.template = '<div class="center-block" style="width:711px;height:400px;"><iframe style="overflow:hidden;height:100%;width:100%" width="100%" height="100%" src="{{url}}" frameborder="0" allowfullscreen></iframe></div>';
     this.link = scope => {
-      scope.url =
-        $sce.trustAsResourceUrl(`http://www.youtube.com/embed/${scope.$parent.home.code}?rel=0&modestbranding=1&showinfo=0`);
+      scope.$watch('code', newVal => {
+        if (newVal) {
+          scope.url = $sce.trustAsResourceUrl(`http://www.youtube.com/embed/${newVal}?rel=0&modestbranding=1&showinfo=0`);
+        }
+      });
     };
   };
 }
